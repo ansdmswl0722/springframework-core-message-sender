2fdcce29b6bdd701c0c1564c7b9fec841038c9c9
muneunji <ansdmswl0722@gmail.com>
2023-04-11 15:36:41 +0900

    commit java Configuration

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class MessageSendService {
     private MessageSender messageSender;
 
+    @Value("${from}")
+    private String from;
     @Autowired
     public void setSmsMessageSender(@SMS("smsMessageSender") MessageSender messageSender) {
         System.out.println("setMessageSender invoked!");
@@ -14,6 +17,7 @@ public class MessageSendService {
 
     public void doSendMessage() {
         User user = new User("ansdmswl0722@gmail.com","01012341234");
-        messageSender.sendMessage(user,"hihi");
+        messageSender.sendMessage(user,from+"hihi");
+
     }
 }
---
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/test/AnotationMain.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.edu.springframework.messagesender.test;
+
+import com.nhnacademy.edu.springframework.messagesender.MessageSendService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AnotationMain {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework.messagesender");
+
+        MessageSendService messageSendService = context.getBean("messageSendService", MessageSendService.class);
+        messageSendService.doSendMessage();
+    }
+}
